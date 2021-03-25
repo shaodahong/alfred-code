c79c20c365041390515e2aed5f9c959353312cd3
shaodahong <darryshaw@gmail.com>
2021-03-25 15:55:00 +0800

    log if open error

--- open.js
+++ open.js
@@ -4,11 +4,8 @@ const arg = process.argv[2];
 
 const codeCommand = process.env.insiders === "1" ? "code-insiders" : "code";
 
-exec(
-  `${codeCommand} -n '${decodeURI(arg.replace("file://", ""))}'`,
-  (err, stdout, stderr) => {
-    if (err) {
-      return;
-    }
+exec(`${codeCommand} -n '${decodeURI(arg.replace("file://", ""))}'`, err => {
+  if (err) {
+    alfy.log(err);
   }
-);
+});
